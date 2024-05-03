7f165ba5f80b5186ab102500061c3a6434df519e
DanielM-04 <dmelotr@uninpahu.edu.co>
2024-05-03 12:46:09 -0500

    Clase de datos

---
+++ Dateclase.py
@@ -0,0 +1,12 @@
+# tipo de dato
+print(type("Hola Mundo")) #tipo str
+
+print(type(2)) #tipo int
+
+print(type(2.1)) #tipo float
+
+print(type(True)) #tipo Boolean
+
+print(type(1 + 1j)) #Tipo COmplejo
+
+#Type es una funcion 
