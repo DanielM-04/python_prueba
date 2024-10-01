e8eaf2902e1a39f0933857977544a341f5fa34d7
DanielM-04 <dmelotr@uninpahu.edu.co>
2024-10-01 12:45:12 -0500

    Nuevos arreglos

---
+++ 01_Variables.py
@@ -0,0 +1,18 @@
+# Variables
+my_variable = "Una variable string"
+print(my_variable)
+print("Hola mundo")
+
+bol_variable = False
+print(bol_variable)
+
+int_variable = 16
+print(int_variable)
+
+
+vble_str_to_int= str(int_variable)
+print("Variable int a str: ",vble_str_to_int)
+print(type(vble_str_to_int))
+
+#concatenacion de variable
+print(my_variable, bol_variable, int_variable)
---
+++ funciones1.py
@@ -0,0 +1,22 @@
+print("Hola mundo")
+u=0
+d=0
+while (u != 1):
+    r=int(input("Ingrese un numero "))
+    if (r==0):
+        u=1
+        break
+
+    d=r+d
+print (d)
+
+
+print("Hola mundo")
+suma= 0
+while (True):
+    r=int(input("Ingrese un numero "))
+    suma += suma + r
+    if (r==-1):
+        break
+print (suma)
+
